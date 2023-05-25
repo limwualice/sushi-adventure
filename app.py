f819cbf3bdb8681eccb6daf30dc96958148c111b
awlim100 <125330688+limwualice@users.noreply.github.com>
2023-05-24 18:42:42 -0700

    created new inputs with pizza restaurants

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import random
-import pickle
+# import pickle
 import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Load the KMeans model
-with open('kmeans.pkl', 'rb') as file:
-    kmeans = pickle.load(file)
+# # Load the KMeans model
+# with open('kmeans.pkl', 'rb') as file:
+#     kmeans = pickle.load(file)
 
 # Load the data frame
-df = pd.read_csv('sushi_to_search.csv')
+df = pd.read_csv('businesses_reviews.csv')
 
 # Define your Flask routes
 @app.route('/')
@@ -20,45 +20,45 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the input from the form
-    rating = request.form.get('rating')
-    reviews = request.form.get('reviews')
-    price = request.form.get('price')
+    stars_x = request.form.get('stars_x')
+    RestaurantsTakeOut = request.form.get('RestaurantsTakeOut')
+    RestaurantsDelivery = request.form.get('RestaurantsDelivery')
+    BusinessAcceptsCreditCards = request.form.get('BusinessAcceptsCreditCards')
+    RestaurantsPriceRange2 = request.form.get('RestaurantsPriceRange2')
+    RestaurantsReservations = request.form.get('RestaurantsReservations')
+    RestaurantsGoodForGroups = request.form.get('RestaurantsGoodForGroups')
+    HasTV = request.form.get('HasTV')
+    GoodForKids = request.form.get('GoodForKids')
+    valet = request.form.get('valet')
 
-    # Validate the input values
-    if not rating or not reviews or not price:
-        if not rating:
-            return "Your rating was wrong", 400
-        elif not reviews:
-            return "Your reviews was wrong", 400
-        elif not price:
-            return "Your price was wrong", 400
-        else:
-            return "something else", 400
+    # Create a dictionary to store the filter conditions
+    filters = {
+        'stars_x': stars_x,
+        'RestaurantsTakeOut': RestaurantsTakeOut,
+        'RestaurantsDelivery': RestaurantsDelivery,
+        'BusinessAcceptsCreditCards': BusinessAcceptsCreditCards,
+        'RestaurantsPriceRange2': RestaurantsPriceRange2,
+        'RestaurantsReservations': RestaurantsReservations,
+        'RestaurantsGoodForGroups': RestaurantsGoodForGroups,
+        'HasTV': HasTV,
+        'GoodForKids': GoodForKids,
+        'valet': valet
+    }
 
-    try:
-        rating = float(rating)
-        reviews = float(reviews)
-        price = float(price)
-    except ValueError:
-        return "Invalid input values", 400
+    # Apply the filters to the DataFrame
+    filtered_df = df.copy()  # Create a copy of the original DataFrame
 
-    # Perform the prediction using the KMeans model
-    cluster = kmeans.predict([[rating, reviews, price]])
+    # Iterate over the filters and apply each one
+    for column, value in filters.items():
+        if value == "True":
+            filtered_df = filtered_df[filtered_df[column] == True]
+        elif value == "I don't care":
+            continue  # Skip the filter for "I don't care"
 
-    # Convert cluster to a scalar value
-    cluster = cluster[0]
+    # Now you have the filtered DataFrame based on the user's input
+    # You can perform further operations or return the results as needed
 
-    # Filter the data frame based on the predicted cluster
-    filtered_df = df[df['Cluster'] == cluster]
-
-    # Get all the sushi restaurants in the filtered cluster
-    restaurants = filtered_df['Name'].tolist()
-
-    # Randomly select 5 sushi restaurants
-    random_restaurants = random.sample(restaurants, k=min(5, len(restaurants)))
-
-    # Render the template with the random 5 sushi restaurants
-    return render_template('result.html', restaurants=random_restaurants)
+    return render_template('result.html', filtered_data=filtered_df)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
---
+++ old/yelp_scrape.py
@@ -0,0 +1,65 @@
+import requests
+import pandas as pd
+from config import ACCESS_TOKEN
+
+def get_reviews(business_id, limit):
+    url = f'https://api.yelp.com/v3/businesses/{business_id}/reviews'
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}'
+    }
+    params = {
+        'limit': limit
+    }
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+    reviews = data['reviews']
+    return reviews
+
+search_url = 'https://api.yelp.com/v3/businesses/search'
+search_params = {
+    'term': 'sushi',
+    'location': 'Los Angeles, CA',
+    'categories': 'restaurants',
+    'limit': 50  # Set a lower limit here
+}
+headers = {
+    'Authorization': f'Bearer {ACCESS_TOKEN}'
+}
+
+business_list = []  # Initialize an empty list to store results
+
+# Make multiple requests with different offsets
+for offset in range(0, 1000, search_params['limit']):
+    
+    search_params['offset'] = offset
+    search_response = requests.get(search_url, headers=headers, params=search_params)
+    search_data = search_response.json()
+    businesses = search_data['businesses']
+
+    # Extract the relevant information from the API response
+    for business in businesses:
+        name = business['name']
+        rating = business['rating']
+        review_count = business['review_count']
+        price = business.get('price', 'N/A')
+        location = business['location']
+
+        # Fetch the reviews for the current business
+        business_id = business['id']
+        reviews = get_reviews(business_id, 100)
+        review_texts = [review['text'] for review in reviews]
+
+        business_list.append({
+            'Name': name,
+            'Rating': rating,
+            'Reviews': review_count,
+            'Price': price,
+            'Location': location,
+            'ReviewsData': review_texts
+        })
+
+# Create a DataFrame from the list of dictionaries
+df = pd.DataFrame(business_list)
+
+# Save the DataFrame
+df.to_csv('sushi_reviews.csv', index=False)
